debc107879306525d263ed6214179830c80ec9d0
yotam1802 <Yotam1802@gmail.com>
2023-09-12 10:15:43 -0600

    Finish Exercise 2.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,42 +1,4 @@
-const Course = ({course}) => {
-  return (
-    <div>
-    <Header header={course.name}/>
-    <Content content={course.parts}/>
-    <Total content={course.parts}/>
-    </div>
-  )
-}
-
-
-const Header = ({header}) => {
-  return <h2>{header}</h2>
-}
-
-const Part = ({name, exercises}) => {
-  return <p>{name} {exercises}</p>
-}
-
-const Content = ({content}) => {
-  return (
-    <div>
-      {content.map(part => 
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      )}
-    </div>
-  )
-}
-
-const Total = ({content}) => {
-  return (
-    <p>
-      <strong>
-      total of {content.reduce((sum, part) => sum + part.exercises, 0)} exercises
-      </strong>
-    </p>
-  )
-}
-
+import Course from './Course'
 
 const App = () => {
   const courses = [
